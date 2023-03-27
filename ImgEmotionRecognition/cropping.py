a8f0568a40731f50add66ca1a3a675695bfccecd
stepi2299 <boenisch22@gmail.com>
2023-03-27 22:11:51 +0200

    adding image cropping task

---
+++ ImgEmotionRecognition/cropping.py
@@ -0,0 +1,25 @@
+import torchvision.io
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import cv2 as cv
+import os
+
+
+def crop_images(video_name, new_folder):
+    frames = torchvision.io.read_video(os.path.join("videos",video_name))[0]
+    mtcnn = MTCNN(image_size=224)
+    os.makedirs(new_folder, exist_ok=True)
+    for i, frame in enumerate(frames):
+        try:
+            box = mtcnn.detect(frame)[0]
+            x, y, x2, y2 = box[0].astype("int16")
+            face = frame.numpy()[y:y2, x:x2]
+            cv.imwrite(os.path.join(new_folder, f"{i}.jpg"), face)
+        except Exception as e:
+            print(f"Exception for {i}: {e}")
+    print(f"Videao has {i} frames")
+
+crop_images("moja_twarz.mp4", "face")
+
+
+
+
